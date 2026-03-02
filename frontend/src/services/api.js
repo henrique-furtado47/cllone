75971a01fa70215f0a91566ea3b3839b2a6a223b
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2026-03-02 10:14:56 -0300

    feat: :passport_control: added teams and memberships api integration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,3 +14,19 @@ export const getTask = (id) => api.get(`tasks/${id}/`)
 export const createTask = (data) => api.post('/tasks/', data)
 export const updateTask = (id, data) => api.put(`task/${id}/`, data)
 export const deleteTask = (id) => api.delete(`tasks/${id}`)
+
+// ==== Teams ====
+
+export const getTeams = () => api.get('/teams/')
+export const getTeam = (id) => api.get(`teams/${id}/`)
+export const createTeam = (data) => api.post('/teams/', data)
+export const updateTeam = (id, data) => api.put(`Team/${id}/`, data)
+export const deleteTeam = (id) => api.delete(`teams/${id}`)
+
+// ==== Memberships ====
+
+export const getMemberships = () => api.get('/memberships/')
+export const createMemberships = (data) => api.post('/memberships/', data)
+export const deleteMemberships = (id) => api.delete(`/memberships/${id}`)
+
+export default api
